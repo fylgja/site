b35ebb6631a641a39c039e9d0b7ca68846193ab9
GrimLink <sean.grimlink@gmail.com>
2021-05-22 12:54:36 +0200

    IMP: formating

--- src/_utils/shortcodes/npm-badges.js
+++ src/_utils/shortcodes/npm-badges.js
@@ -3,21 +3,27 @@ module.exports = function npmBadges(npmUrl, classes = "npm-badges") {
     // Since the badge content can be diffrent in size
     const badgeStyle = "flat-square";
     let badges = "";
-    badges += `<a href="https://www.npmjs.org/package/${npmUrl}" class="no-underline text-reset">
+    badges += `<a href="https://www.npmjs.org/package/${npmUrl}"
+        class="no-underline text-reset"
+        target="_blank"
+        rel="noopener noreferrer">
         <img src="https://img.shields.io/npm/v/${npmUrl}?style=${badgeStyle}"
-        alt="NPM version"
-        width="80"
-        height="20"
-        loading="eager"
-        style="width: auto;">
+            alt="NPM version"
+            width="80"
+            height="20"
+            loading="eager"
+            style="width: auto;">
     </a>`;
-    badges += `<a href="https://www.npmjs.org/package/${npmUrl}" class="no-underline text-reset">
+    badges += `<a href="https://www.npmjs.org/package/${npmUrl}"
+        class="no-underline text-reset"
+        target="_blank"
+        rel="noopener noreferrer">
         <img src="https://img.shields.io/npm/dm/${npmUrl}?style=${badgeStyle}"
-        alt="NPM downloads"
-        width="140"
-        height="20"
-        loading="eager"
-        style="width: auto;">
+            alt="NPM downloads"
+            width="140"
+            height="20"
+            loading="eager"
+            style="width: auto;">
     </a>`;
 
     return `<div class="${classes}">${badges}</div>`;
