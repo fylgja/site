4cf5fb0cfd9c32dafb5208a501c2661ab238d175
GrimLink <sean.grimlink@gmail.com>
2021-09-29 13:08:55 +0200

    IMP: move theme toggle to the body for faster loading of the initial style

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,3 @@
-import { themeToggle } from "./theme-toggle";
 import { newDialog } from "./dialog";
 import dialogPolyfill from "dialog-polyfill/dist/dialog-polyfill.js";
 
@@ -8,10 +7,6 @@ newDialog("#offcanvas-menu", "#show-offcanvas-menu");
 if (typeof HTMLDialogElement !== "function") {
     dialogPolyfill.registerDialog("#offcanvas-menu");
 }
-
-document.addEventListener("click", (event) => {
-    themeToggle(event);
-});
 
 window.addEventListener("load", () => {
     if ("serviceWorker" in navigator) {
