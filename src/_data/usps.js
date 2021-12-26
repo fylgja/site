f64f33a942b2d9f73759a700cdcf04503ea04e73
GrimLink <sean.grimlink@gmail.com>
2021-12-26 18:04:38 +0100

    IMP: homepage style

--- src/_data/usps.js
+++ src/_data/usps.js
@@ -3,32 +3,37 @@ module.exports = {
         {
             title: "Modularity",
             content:
-                "Fylgja is build with modularity in mind, import only the parts you need,<br>keep your bundle size and CSS small without using any extra tools.",
+                "Fylgja is build with modularity in mind, import only the parts you need,<br>keep your bundle size and CSS small without using any extra tools, like PurgeCSS.",
             icon: "widgets",
+            iconStyle: "--avatar-bg: var(--color-alt);",
         },
         {
             title: "Ease to edit and to add your own style",
             content:
-                "Use Fylgja as a solid basis to build your own styles with minimal effort,<br>thanks to SCSS and/or CSS Variables and CSS Utilities.",
+                "Use Fylgja as a solid basis to build your own styles with minimal effort,<br>thanks to SCSS and/or CSS Variables",
             icon: "palette",
+            iconStyle: "--avatar-bg: #ffa726;",
         },
         {
             title: "RTL out of the box",
             content:
                 "Fylgja is made with writing direction in mind,<br>so it works with Languages that use a different direction than LTR.",
             icon: "format_textdirection_rtl",
+            iconStyle: "--avatar-bg: #66bb6a;",
+        },
+        {
+            title: "Ease to start with the Fylgja Design Tokens and CSS Utilities",
+            content:
+                "Fylgja comes with a set of Design Tokens and CSS Utilities offered trough Javascript, SCSS and CSS.",
+            icon: "style",
+            iconStyle: "--avatar-bg: #d32f2f;",
         },
         {
             title: "Web Accessibility in mind",
             content:
                 "Each Fylgja component is built with accessibility in mind,<br>and Encourages the use of the right HTML syntax.",
             icon: "visibility",
-        },
-        {
-            title: "No JavaScript required",
-            content:
-                "Fylgja is CSS only, so you can integrate it in any JS environment,<br>without having to worry it conflicts with something.",
-            icon: "css",
+            iconStyle: "--avatar-bg: #03a9f4;",
         },
     ],
 };
