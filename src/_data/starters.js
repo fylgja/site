3cfca00dd1cee48972bc95e5e3b09834f04ce4aa
grimlink <sean.grimlink@gmail.com>
2022-05-23 03:36:11 +0200

    ADD: 11ty starter

---
+++ src/_data/starters.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        url: "https://github.com/fylgja/11ty-starter-template",
+        title: "11ty Starter Template",
+        description: "11ty starter template build using Fylgja CSS.",
+        thumb: "starter-thumb/11ty.jpg",
+    },
+];
