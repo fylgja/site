b657bf56c70c00b23ded4d44c76bc3acc0141159
grimlink <sean.grimlink@gmail.com>
2022-10-05 21:48:34 +0200

    ADD: placeholder for more starters

--- src/_data/starters.js
+++ src/_data/starters.js
@@ -6,4 +6,10 @@ module.exports = [
         thumb: "starter-thumb/11ty.jpg",
         previewUrl: "https://11ty-fylgja.netlify.app/",
     },
+    // {
+    //     url: "https://github.com/fylgja/parcel-starter-template",
+    //     title: "ParcelJS Starter Template",
+    //     description: "ParcelJS starter template build using Fylgja CSS.",
+    //     thumb: "starter-thumb/parcel.jpg",
+    // },
 ];
