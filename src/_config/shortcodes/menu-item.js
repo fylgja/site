f7146caccfca8edc767c4519ad8751aedeb4169f
grimlink <sean.grimlink@gmail.com>
2023-02-09 02:07:00 +0100

    IMP: ext links

--- src/_config/shortcodes/menu-item.js
+++ src/_config/shortcodes/menu-item.js
@@ -1,17 +1,18 @@
-module.exports = function menuItem(
-    content,
-    url,
-    className = "",
-    isExternal = false,
-    pageUrl = ""
-) {
+module.exports = function menuItem(content, url, options) {
+    const {
+        classList = "",
+        isExternal = false,
+        pageUrl = "",
+        rel = "",
+    } = options;
+
     const isActive = pageUrl === url;
     const current = isActive ? 'aria-current="page"' : "";
-    const external = isExternal
-        ? 'rel="noopener noreferrer" target="_blank"'
-        : "";
-    let classes = className;
+    const relValue = rel + (isExternal ? "nofollow" : "");
+    let classes = classList;
     if (isActive) classes += " is-active";
 
-    return `<a href="${url}" class="${classes}" ${current} ${external}>${content}</a>`;
+    return `<a href="${url}" ${current}${relValue && ` rel="${relValue}"`}${
+        classes && ` class="${classes}"`
+    }>${content}</a>`;
 };
