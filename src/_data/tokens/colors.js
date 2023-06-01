80abbb0c26d77b07be8642314cdbc62974b55dd1
grimlink <sean.grimlink@gmail.com>
2023-06-01 07:23:15 +0200

    ADD: new colors to docs
    needs more work

--- src/_data/tokens/colors.js
+++ src/_data/tokens/colors.js
@@ -1,19 +1,94 @@
 const colors = require("@fylgja/colors");
 
-module.exports = {
-    keys: Object.keys(colors),
-    gray: Object.values(colors.gray),
-    blueGray: Object.values(colors.blueGray),
-    red: Object.values(colors.red),
-    pink: Object.values(colors.pink),
-    purple: Object.values(colors.purple),
-    violet: Object.values(colors.violet),
-    indigo: Object.values(colors.indigo),
-    blue: Object.values(colors.blue),
-    cyan: Object.values(colors.cyan),
-    teal: Object.values(colors.teal),
-    green: Object.values(colors.green),
-    lime: Object.values(colors.lime),
-    yellow: Object.values(colors.yellow),
-    orange: Object.values(colors.orange),
+const contrast = [
+    ".02",
+    ".04",
+    ".12",
+    ".18",
+    ".2",
+    ".22",
+    ".21",
+    ".2",
+    ".18",
+    ".14",
+    ".12",
+    ".08",
+    ".04",
+];
+const light = [
+    "99%",
+    "94%",
+    "88%",
+    "80%",
+    "74%",
+    "67%",
+    "59%",
+    "52%",
+    "44%",
+    "36%",
+    "24%",
+    "18%",
+    "10%",
+];
+const colorToShow = [
+    "red-gray",
+    "green-gray",
+    "blue-gray",
+    "gray",
+    "pink",
+    "violet",
+    "purple",
+    "indigo",
+    "blue",
+    "cyan",
+    "teal",
+    "green",
+    "lime",
+    "yellow",
+    "orange",
+    "red",
+];
+
+const colorsMap = {
+    keys: colorToShow,
+    "red-gray": light.map((weight) => `oklch(${weight} 0.03 18)`),
+    "green-gray": light.map((weight) => `oklch(${weight} 0.03 152)`),
+    "blue-gray": light.map((weight) => `oklch(${weight} 0.03 258)`),
+    gray: light.map((weight) => `oklch(${weight} 0 0)`),
+    pink: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 350)`
+    ),
+    purple: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 330)`
+    ),
+    violet: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 310)`
+    ),
+    indigo: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 290)`
+    ),
+    cyan: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 220)`
+    ),
+    teal: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 170)`
+    ),
+    green: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 140)`
+    ),
+    lime: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 125)`
+    ),
+    blue: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 260)`
+    ),
+    yellow: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 100)`
+    ),
+    orange: light.map(
+        (weight, index) => `oklch(${weight} ${contrast[index]} 70)`
+    ),
+    red: light.map((weight, index) => `oklch(${weight} ${contrast[index]} 30)`),
 };
+
+module.exports = colorsMap;
