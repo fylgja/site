42cd0e52501bb6dded70dfcd2d5a1faeff547c79
grimlink <sean.grimlink@gmail.com>
2023-08-04 14:43:41 +0200

    IMP: colors doc

--- src/_data/tokens/colors.js
+++ src/_data/tokens/colors.js
@@ -1,27 +1,16 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
-// const colors = require("@fylgja/colors/hsl");
-const colors = require("@fylgja/colors/oklch-static");
+const oklch = require("@fylgja/colors/oklch-static");
+const hsl = require("@fylgja/colors/hsl");
+const hex = require("@fylgja/colors/hex");
 
-const colorsMap = {
-    keys: Object.keys(colors),
-    redGray: Object.values(colors.redGray),
-    greenGray: Object.values(colors.greenGray),
-    blueGray: Object.values(colors.blueGray),
-    gray: Object.values(colors.gray),
-    pink: Object.values(colors.pink),
-    purple: Object.values(colors.purple),
-    violet: Object.values(colors.violet),
-    indigo: Object.values(colors.indigo),
-    cyan: Object.values(colors.cyan),
-    teal: Object.values(colors.teal),
-    green: Object.values(colors.green),
-    lime: Object.values(colors.lime),
-    blue: Object.values(colors.blue),
-    yellow: Object.values(colors.yellow),
-    orange: Object.values(colors.orange),
-    red: Object.values(colors.red),
+const colors = {
+    keys: Object.keys(oklch),
+    count: Object.keys(oklch.gray).length,
+    oklch,
+    hsl,
+    hex,
 };
 
-module.exports = colorsMap;
+module.exports = colors;
