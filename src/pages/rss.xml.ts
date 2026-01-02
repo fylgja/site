89a56a55b63f020b08029cab81fa1e3c64c858c4
grimlink <sean.grimlink@gmail.com>
2026-01-02 19:45:10 +0100

    FIX: order

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,11 +5,16 @@ import siteConfig from "site.config";
 export async function GET(context: any) {
 	const blog = await getCollection("blog");
 
+	const collection = blog.sort(
+		(a, b) =>
+			Date.parse(b.data.publishDate.toString()) - Date.parse(a.data.publishDate.toString()),
+	);
+
 	return rss({
 		site: context.site,
 		title: `The ${siteConfig.name} Blog`,
 		description: `News and updates about ${siteConfig.name}`,
-		items: blog.map((post) => ({
+		items: collection.map((post) => ({
 			title: post.data.title,
 			description: post.data.description,
 			pubDate: post.data.publishDate,
