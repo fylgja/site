b963ac3dfe6a9456deab7a7b2debc61ec8bf542e
grimlink <sean.grimlink@gmail.com>
2026-04-14 11:39:15 +0200

    FIX: Remove redundant content from RSS feed style

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import siteConfig from "site.config";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-
-const parser = new MarkdownIt();
 
 export async function GET(context: any) {
 	const blog = await getCollection("blog", ({ data }) => {
@@ -16,26 +12,19 @@ export async function GET(context: any) {
 			Date.parse(b.data.publishDate.toString()) - Date.parse(a.data.publishDate.toString()),
 	);
 
-	const items = collection.map((post) => ({
-		title: post.data.title,
-		description: post.data.description,
-		pubDate: post.data.publishDate,
-		link: `/blog/${post.id}/`,
-		author: post.data.author.name,
-		categories: post.data.tags,
-		content: sanitizeHtml(parser.render(post.body || ""), {
-			allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-		}),
-	}));
-
-	// console.log(items);
-
 	return rss({
 		site: context.site,
 		title: `The ${siteConfig.name} Blog`,
 		description: `News and updates about ${siteConfig.name} and more...`,
 		stylesheet: "/rss-style.xsl",
 		customData: `<language>en-us</language>`,
-		items,
+		items: collection.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.publishDate,
+			link: `/blog/${post.id}/`,
+			author: post.data.author.name,
+			categories: post.data.tags,
+		})),
 	});
 }
